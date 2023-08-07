22500c20dc9d5af5d13a3c95a28229df7a19dc3a
Nicolain12 <lainnicolas1@gmail.com>
2023-08-07 20:23:15 -0300

    brandsProducts logic

--- src/components/brandsProducts/brandsProducts.jsx
+++ src/components/brandsProducts/brandsProducts.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './brandsProducts.css'
+import { useParams } from 'react-router-dom';
+import { fetchApi } from "../../modules/mainModules";
+import appInfo from "../../modules/appInfo";
+
+
 
 function brandsProducts() {
+  const { id } = useParams();
+  const [brandProducts, setBrandProducts] = useState([])
 
-return (
-  <div className="a">
-    <h1>Hola</h1>
-  </div>
+  useEffect(() => {
+    fetchApi(`${appInfo.root}/cars/brands/${id}`, {
+      method: 'GET',
+    }, (resolve, reject) => {
+      if(reject){console.log(reject);}else{
+        console.log(resolve.info.carsIncluded)
+      }
+    });
+  }, []);
+
+ 
+
+  return (
+    <div className="a">
+      <h1>Hola</h1>
+    </div>
   )
 }
 
