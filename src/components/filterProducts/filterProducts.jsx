d4dd268af6e8fbd6f13960347bb303fce078502c
Maxipar03 <maxipar2003@gmial.com>
2023-09-29 02:21:25 -0300

    [200~

--- src/components/filterProducts/filterProducts.jsx
+++ src/components/filterProducts/filterProducts.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./filterProducts.css"
 import { fetchApi } from "../../modules/mainModules"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown, faAngleUp, faArrowRight } from '@fortawesome/free-solid-svg-icons';;
 import appInfo from "../../modules/appInfo";
 import banner from "../../../public/ImageTestbannerfilter.jpg"
 // import { loadConfigFromFile } from "vite";
@@ -16,6 +16,36 @@ function filterProducts(props) {
     const [expandedBrands, setExpandedBrands] = useState({});
 
     const [selectedBrands, setSelectedBrands] = useState([]);
+
+    const [brandsActive, setBrandsActive] = useState(false);
+
+    const [modelsActive, setModelsActive] = useState(false);
+
+    const[bodyCarActive,setBodyCarActive] = useState(false)
+
+    const toggleBrands = () => {
+        setBrandsActive(!brandsActive);
+        setModelsActive(false);
+        setBodyCarActive(false)
+      };
+    
+    const toggleBodyCars = () => {
+        setBrandsActive(false);
+        setModelsActive(false);
+        setBodyCarActive(!bodyCarActive)
+    }
+
+    const toggleModels = () => {
+        setModelsActive(!modelsActive);
+        setBrandsActive(false);
+        setBodyCarActive(false)
+      };
+    
+    const resetFilters = () => {
+        setBrandsActive(false);
+        setModelsActive(false)
+        setBodyCarActive(false)
+      }
 
     const toggleBrand = (brandId) => {
         setExpandedBrands((prevExpandedBrands) => ({
@@ -64,10 +94,13 @@ function filterProducts(props) {
     console.log(models);
 
     return (
-        <div className="filterComponent">
-            <div className="filterContainer">
+        <div  className={`filterComponent ${!(brandsActive || modelsActive || bodyCarActive) ? 'active' : ''}`}>
+            <div className={`filterContainer ${!(brandsActive || modelsActive || bodyCarActive) ? 'active' : ''}`}>
+                {!(brandsActive || modelsActive || bodyCarActive) && (
                 <h2 className="filterTitle">Filter</h2>
+                )}
                 {/* YEAR */}
+                {!(brandsActive || modelsActive || bodyCarActive) && ( 
                 <div className="filterDate">
                     <h4 className="filterName">Year</h4>
                     <div className="dateContainer">
@@ -81,6 +114,9 @@ function filterProducts(props) {
                         </div>
                     </div>
                 </div>
+                )}
+                {/* KM */}
+                {!(brandsActive || modelsActive || bodyCarActive) && (
                 <div className="filterKM">
                     <h4 className="filterName">Kilometres</h4>
                     <div className="kmContainer">
@@ -94,6 +130,9 @@ function filterProducts(props) {
                         </div>
                     </div>
                 </div>
+                )}
+                {/* PRICE */}
+                {!(brandsActive || modelsActive || bodyCarActive) && (
                 <div className="filterKM">
                     <h4 className="filterName">Price</h4>
                     <div className="priceContainer">
@@ -107,6 +146,9 @@ function filterProducts(props) {
                         </div>
                     </div>
                 </div>
+                )}
+                {/* COLOR */}
+                {!(brandsActive || modelsActive || bodyCarActive) && (
                 <div className="filterColor">
                     <h4 className="filterName">Color</h4>
                     <div>
@@ -118,17 +160,32 @@ function filterProducts(props) {
                         <span className="circle blue"></span>
                     </div>
                 </div>
+                )}
+                {/* TRANSMISION */}
+                {!(brandsActive || modelsActive || bodyCarActive) && (
                 <div className="filterTrasmision">
-                <select className="inputPrice">
+                <h4 className="filterName">Transmision</h4>
+                <select className="inputTransmision">
                 <option disabled selected className="puto">Select Option</option>
                 <option>Manual</option>
                 <option>Automatic</option>
                 </select>
                 </div>
+                )}
                 {/*BODYCAR*/}
                 {props.bodyCar ? <div>
-                    <h4 className="filterName">BodyCar</h4>
-                    <div className="modelsInputDiv">
+                    {!(brandsActive || modelsActive || bodyCarActive) && (
+                    <div>
+                    <h4 onClick={toggleBodyCars} className="filterNameOption">BodyCar</h4>
+                    </div>
+                    )}
+                    {bodyCarActive && (
+                    <div className={`modelsInputDiv${bodyCarActive ? 'active' : ''}`}>
+                        <div className="filterOptions">
+                        {/*<button onClick={resetFilters} className="exitOptions">Go Back</button>*/}
+                        <div onClick={resetFilters} className="arrow prev" />
+                        <h4 className="filterNameSelect">BodyCar</h4>
+                        </div>
                         <div className="scrollable-content">
                             {props.bodyCar.map((bodyCar) => (
                               <div
@@ -137,15 +194,23 @@ function filterProducts(props) {
                           >
                               <input type="checkbox" className="checkBoxBrand" id="" />
                               <p>{bodyCar.name}</p>
-                          </div>
+                             </div>
                             ))}
                         </div>
                     </div>
+                      )}
                 </div> : null}
                 {/* BRANDS */}
                 {props.brands ? <div>
-                    <h4 className="filterName">Brand</h4>
+                    {!(brandsActive || modelsActive || bodyCarActive) && (
+                    <h4 onClick={toggleBrands} className="filterNameOption">Brand</h4>
+                    )}
+                 {brandsActive && (
                     <div className="modelsInputDiv">
+                        <div className="filterOptions">
+                        <div onClick={resetFilters} className="arrow prev" />
+                        <h4 onClick={toggleBrands} className="filterNameSelect">Brand</h4>
+                        </div>
                         <div className="scrollable-content">
                             {props.brands.map((brand) => (
                               <div
@@ -160,11 +225,18 @@ function filterProducts(props) {
                             ))}
                         </div>
                     </div>
-
+                    )}
                 </div> : null}
                 {/* MODELS */}
-                <h4 className="filterName">Model</h4>
+                {!(brandsActive || modelsActive || bodyCarActive) && (
+                <h4 onClick={toggleModels} className="filterNameOption">Model</h4>
+                )}
+                {modelsActive && (
                 <div className="modelsInputDiv">
+                    <div className="filterOptions">
+                    <div onClick={resetFilters} className="arrow prev" />
+                    <h4 onClick={toggleModels} className="filterNameSelect">Model</h4>
+                    </div>
                     {modelsRender ?
                         <div className="scrollable-content">
                             {models.map((modelBrand) => (
@@ -194,6 +266,9 @@ function filterProducts(props) {
                             ))}
                         </div> : null}
                 </div>
+                )}
+                {/* ON SALE */}
+                {!(brandsActive || modelsActive || bodyCarActive) && (
                 <div className="onSaleFilter">
                     <h4>On sale</h4>
                     <div className="checkbox-wrapper-64">
@@ -204,6 +279,7 @@ function filterProducts(props) {
                         </label>
                     </div>
                 </div>
+                )}
             </div>
         </div>
     )
